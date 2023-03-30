6a606a1ced6c54b0f49bd7da16fb5cf3f6556796
LeandroAgustinFernandez <leandroa.fernandez@gmail.com>
2023-03-30 16:33:21 -0300

    Entrega Desafio Clase06

---
+++ src/ProductManager.js
@@ -0,0 +1,85 @@
+import fs from "fs";
+
+export default class ProductManager {
+  constructor(path) {
+    this.path = path;
+  }
+
+  async addProduct(title, description, price, thumbnail, code, stock) {
+    let products = await this.getProducts();
+    try {
+      if (this.#checkMandatoryFields([...arguments]))
+        throw new Error(`All the fields are mandatory.`);
+      if (this.#checkIfCodeExists(products, code))
+        throw new Error(`The product code already exists.`);
+      let id = products[products.length - 1]?.id + 1 || 1;
+      const newProduct = {
+        id,
+        title,
+        description,
+        price,
+        thumbnail,
+        code,
+        stock,
+      };
+      products.push(newProduct);
+      await fs.promises.writeFile(this.path, JSON.stringify(products, null, 2));
+    } catch (error) {
+      return { error: `${error.message}` };
+    }
+  }
+
+  #checkIfCodeExists(products, code) {
+    return products.find((product) => product.code === code);
+  }
+
+  #checkMandatoryFields(fields) {
+    if (fields.length !== 6) return true;
+    return fields.some((field) => field === "" || field === undefined);
+  }
+
+  async getProducts(limit = false) {
+    if (fs.existsSync(this.path)) {
+      let products = await fs.promises.readFile(this.path, "utf-8");
+      return limit === false
+        ? JSON.parse(products)
+        : JSON.parse(products).splice(0, limit);
+    } else {
+      return [];
+    }
+  }
+
+  async getProductById(id) {
+    let products = await this.getProducts();
+    let product = products.find((product) => product.id === parseInt(id));
+    try {
+      if (!product) throw new Error(`Product not found`);
+      return product;
+    } catch (error) {
+      return { error: `${error.message}` };
+    }
+  }
+
+  async updateProduct(id, newParams) {
+    if (Object.keys(newParams).includes("id")) delete newParams.id;
+    let products = await this.getProducts();
+    products = products.map((product) => {
+      if (product.id === id) product = { ...product, ...newParams };
+      return product;
+    });
+    await fs.promises.writeFile(this.path, JSON.stringify(products, null, 2));
+  }
+
+  async deleteProduct(id) {
+    let products = await this.getProducts();
+    try {
+      let productExist = products.find((product) => product.id === id);
+      if (!productExist) throw new Error(`Product doesn't exist.`);
+      products.splice(products.indexOf(productExist), 1);
+      await fs.promises.writeFile(this.path, JSON.stringify(products, null, 2));
+      return `The product was successfully removed`;
+    } catch (error) {
+      return { error: `${error.message}` };
+    }
+  }
+}
---
+++ src/app.js
@@ -0,0 +1,35 @@
+import ProductManager from "./ProductManager.js";
+import express from "express";
+const app = express();
+
+const productManager = new ProductManager("./products.json");
+
+app.get("/", (request, response) => {
+  response.send(`<h1>Bienvenido a la tienda de productos</h1>`);
+});
+
+app.get("/products", async (request, response) => {
+  let { limit } = request.query;
+  let products = await productManager.getProducts(limit);
+  response.send({ products });
+});
+
+app.get("/products/:pid", async (request, response) => {
+  let { pid } = request.params;
+  let product = await productManager.getProductById(pid);
+  response.send({ product });
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}
+  
+  Testing:
+
+  - Todos los productos: http://localhost:${PORT}/products
+  - 5 Primeros productos: http://localhost:${PORT}/products?limit=5
+  - Producto con id=2: http://localhost:8080/products/2
+  - Producto no existe: http://localhost:8080/products/34123123
+  `);
+});
